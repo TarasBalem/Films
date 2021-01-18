2f26cce0f94e90c7e3cb9f86ca40fd10ae301c7a
TarasBalem <balemtaras@gmail.com>
2021-01-18 11:16:00 +0200

    fix featured test

--- src/components/__tests__/Featured.spec.jsx
+++ src/components/__tests__/Featured.spec.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import FilmContext from "contexts/FilmContext";
+import FilmContext, * as filmsFuncs from "contexts/FilmContext";
 import Featured from "components/Featured";
 
+const mockState = {films: [{_id: 1, featured: false}]};
+const mockDispatch = jest.fn();
+const mockContext = [mockState, mockDispatch];
+
+jest.spyOn(filmsFuncs, "toggleFeatured");
+filmsFuncs.toggleFeatured.mockImplementation(() => jest.fn());
+
 const propsData = {id: "1", featured: true};
-const toggleFeatured = jest.fn();
 
 const RenderComponent = props => {
   return (
-    <FilmContext.Provider value={{toggleFeatured}}>
+    <FilmContext.Provider value={mockContext}>
       <Featured {...props} />
     </FilmContext.Provider>
   );
@@ -25,8 +31,12 @@ test("should render correct", () => {
   expect(iconEl).not.toHaveClass("empty");
 
   userEvent.click(spanEl);
-  expect(toggleFeatured).toHaveBeenCalledTimes(1);
-  expect(toggleFeatured).toHaveBeenCalledWith("1");
+  expect(filmsFuncs.toggleFeatured).toHaveBeenCalledTimes(1);
+  expect(filmsFuncs.toggleFeatured).toHaveBeenCalledWith(
+    mockState,
+    mockDispatch,
+    "1",
+  );
 
   propsData["featured"] = false;
   rerender(<RenderComponent {...propsData} />);
